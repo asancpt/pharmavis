94f87d51187b3ba6a96c4dfe6e096057d173e01c
Sungpil Han CPT <shan@acp.kr>
2018-02-20 17:14:38 +0900

    update:v 0.0.2

--- R/ggnca.R
+++ R/ggnca.R
@@ -18,7 +18,6 @@
 #' @examples
 #' plot_nca(concData = Theoph, colSubj = "Subject", colTime = "Time", colConc = "conc")
 #' plot_nca(concData = Indometh, colSubj = "Subject", colTime = "time", colConc = "conc")
-#'
 
 plot_nca <- function(concData, colSubj = "Subject", colTime = "Time", colConc = "conc"){
 #concData = Theoph; colSubj = "Subject"; colTime = "Time"; colConc = "conc"
@@ -26,7 +25,7 @@ plot_nca <- function(concData, colSubj = "Subject", colTime = "Time", colConc = 
     concData <- data.frame(concData)
     ggncaDataset <- data.frame(Subject = concData[ , colSubj],
                                Time = concData[ , colTime],
-                               conc = concData[ , colConc])
+                               conc = concData[ , colConc], stringsAsFactors = FALSE)
     group_log <- ggplot(ggncaDataset,
                        aes(Time, conc, group = Subject)) +
         geom_line() +
@@ -43,3 +42,26 @@ plot_nca <- function(concData, colSubj = "Subject", colTime = "Time", colConc = 
         cowplot::draw_plot_label(c("A", "B", "C"), c(0, 0, 0.5), c(1, 0.5, 0.5), size = 15)
     return(cowReturn)
 }
+
+
+#' concentration-time curves
+#'
+#' This draws pharmacokinetic concentration-time curve with ggplot2 packages.
+#' 
+#' @param df concentration data table
+#' @param colSubj column name for subject ID
+#' @param colTime column name for time
+#' @param colConc column name for concentration
+#'
+
+
+plot_conc <- function(df) {
+  df %>% 
+    ggplot(aes(x = time, y = conc, color = subjid, group = interaction(subjid, day))) +
+    geom_line() +
+    geom_point() +
+    scale_x_continuous(breaks = seq(0, 24, by = 4)) +
+    labs(x = 'Time (hr)', y = 'Concentration (ng/mL)', color = 'Subject') +
+    theme_bw()
+}
+
